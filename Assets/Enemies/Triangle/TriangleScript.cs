a8e936e14c576d3c91d08a76448e7851ede15b88
UserTCQ <rayngu2005@gmail.com>
2020-07-15 18:58:45 +0700

    new colors

--- Assets/Enemies/Triangle/TriangleScript.cs
+++ Assets/Enemies/Triangle/TriangleScript.cs
@@ -9,7 +9,7 @@ public class TriangleScript : MonoBehaviour, ISpawnedObject
         transform.localPosition = new Vector2(float.Parse(args[0]), float.Parse(args[1]));
         transform.localScale = new Vector2(float.Parse(args[2]), float.Parse(args[3]));
         transform.localRotation = Quaternion.Euler(0, 0, float.Parse(args[4]));
-        GetComponent<SpriteRenderer>().color = new Color32(255, 32, 113, byte.Parse(args[5]));
-        GetComponent<BoxCollider2D>().enabled = bool.Parse(args[6]);
+        GetComponent<SpriteRenderer>().color = new Color32(byte.Parse(args[5]), byte.Parse(args[6]), byte.Parse(args[7]), byte.Parse(args[8]));
+        GetComponent<BoxCollider2D>().enabled = bool.Parse(args[9]);
     }
 }
